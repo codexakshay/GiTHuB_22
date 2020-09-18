373e1bd2c6ad635827c1be29a14b0f93173f3976
codexakshay <codexakshay@gmail.com>
2020-09-18 18:54:42 +0530

    UC_GiTHuB_21

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,10 +1,10 @@
 public class EmployeeWage {
-    public static final int WAGE_PER_HR=20,PART_TIME=1,FULL_DAY=2,MONTH=20;
+    public static final int WAGE_PER_HR=20,PART_TIME=1,FULL_DAY=2,MONTH=20,MAX_HRS=100;
     public static void main(String[] args)
     {
         int salary=0,workingHr=0,day=0;
         int totalWorkingHrs=0,totalSalary=0;
-        while(day<=MONTH)
+        while(day<=MONTH && totalWorkingHrs<MAX_HRS)
         {
             int random = (int) Math.floor(Math.random() * 10) % 3;
             switch (random) {
@@ -17,12 +17,21 @@ public class EmployeeWage {
                 default:
                     break;
             }
+            if(totalWorkingHrs==96 || totalWorkingHrs == 98)
+            {
+                if (workingHr==8)
+                    break;
+            }
+            else if(totalWorkingHrs==98)
+            {
+                break;
+            }
             salary=WAGE_PER_HR*workingHr;
             totalWorkingHrs+=workingHr;
             totalSalary+=salary;
             day++;
         }
         System.out.println("EMPLOYEE WAGE : " + totalSalary);
+        System.out.println("Days "+day+" Hours "+totalWorkingHrs);
     }
 }
-
