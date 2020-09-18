3b9dce301b6fb769208503a5e8ec69717827b83a
AKSHAY <70620976+codexakshay@users.noreply.github.com>
2020-09-18 20:14:19 +0530

    Update EmployeeWage.java

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -37,11 +37,17 @@ public class EmployeeWage extends WageCalculation
 {
                                 public static void main(String[] args)
                                 {
-                                        System.out.println("\nEnter Company Name : ");
+                                        System.out.println("\nHow many Companies you want to check ?");
+                                        Scanner scn = new Scanner(System.in);
+                                        int temp = scn.nextInt();
+                                        for(int i=1;i<=temp;i++)
+                                        {
                                         Scanner sc=new Scanner(System.in);
+                                        System.out.println("\nEnter Company Name : ");
                                         String CompanyName=sc.nextLine();
                                         System.out.println("Enter Number of Working Days : ");
                                         int NumberOfWorkingDays=sc.nextInt();
                                         calculateEmpWage(CompanyName,NumberOfWorkingDays,20,30);
+                                        }
                                 }
 }
